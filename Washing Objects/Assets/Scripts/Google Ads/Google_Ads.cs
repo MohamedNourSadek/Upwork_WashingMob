a3ff257f2441e39b8e7dfed6cdb286ee7e2126ba
Mohamed Nour <mohamednoursadek@gmail.com>
2021-11-16 17:47:26 +0200

    Google_Ads

---
+++ Washing Objects/Assets/Scripts/Google Ads/Google_Ads.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+
+public class Google_Ads : MonoBehaviour
+{
+    InterstitialAd ad;
+
+    void Start()
+    {
+        MobileAds.Initialize(InitializationStatus => { });
+    }
+
+    void Request_Ad()
+    {
+        string Ad_ID = "ca-app-pub-5755293822993295/9618739726";
+        
+        if (ad != null)
+            ad.Destroy();
+
+        ad = new InterstitialAd(Ad_ID);
+        ad.LoadAd(new AdRequest.Builder().Build());
+
+    }
+
+    public void Show_AD()
+    {
+        Request_Ad();
+
+        if (ad.IsLoaded())
+            ad.Show();
+        else
+            Debug.Log("Error");
+    }
+
+
+}
