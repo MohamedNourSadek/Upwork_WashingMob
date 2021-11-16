7257a0ebadb56a9a0e2e677a0bafd4a4a65c8af8
Mohamed Nour <mohamednoursadek@gmail.com>
2021-11-16 18:51:08 +0200

    Google/Unity Fixed

--- Washing Game/Assets/Scripts/Google Ads/Google_Ads.cs
+++ Washing Game/Assets/Scripts/Google Ads/Google_Ads.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
 using System;
+using UnityEngine.UI;
 
 public class Google_Ads : MonoBehaviour
 {
     InterstitialAd ad;
-
+    public Text t;
+    
     void Start()
     {
         MobileAds.Initialize(InitializationStatus => { });
     }
-
+    
     void Request_Ad()
     {
         string Ad_ID = "ca-app-pub-5755293822993295/9618739726";
@@ -30,9 +32,15 @@ public class Google_Ads : MonoBehaviour
         Request_Ad();
 
         if (ad.IsLoaded())
+        {
             ad.Show();
+            t.text = "Success";
+        }
         else
+        {
             Debug.Log("Error");
+            t.text = "Fail";
+        }
     }
 
 
